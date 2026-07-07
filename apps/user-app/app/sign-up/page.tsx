ff2384943900c3b9d15cbaf545872c05b15068c7
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-07-08 02:04:13 +0530

    basic layout

--- apps/user-app/app/sign-up/page.tsx
+++ apps/user-app/app/sign-up/page.tsx
@@ -1,6 +1,62 @@
+import Link from "next/link";
+
 export default function signUp(){
     return(
         <div>
+            <div className="flex flex-row items-center justify-between px-2 border-b-2 border-b-white/40 h-15">
+                <Link href={"/"}>
+                    <div className="text-2xl font-bold">
+                        PAYTM
+                    </div>
+                </Link>
+                <Link href={"/sign-in"}>
+                    <button className="border-2 bg-white text-black p-2 font-bold rounded-xl h-10 w-25">
+                        Sign In
+                    </button>
+                </Link>
+            </div>
+
+            <div className="flex items-center justify-center min-h-[calc(100vh-60px)]">
+                <div className="flex flex-col gap-3 w-100 h-100 border-2 border-white p-10 rounded-2xl">
+                    <div className = "flex flex-col gap-1">
+                        <h1>Name</h1>
+                        <input
+                            type="text"
+                            placeholder="enter your name"
+                            className="border-2 border-white h-10 w-70 p-3 rounded-xl"
+
+                        />
+                    </div>
+                    <div className = "flex flex-col gap-1">
+                        <h1>Number</h1>
+                        <input
+                            type="text"
+                            placeholder="enter phone number"
+                            className="border-2 border-white h-10 w-70 p-3 rounded-xl"
+
+                        />
+                    </div>
+                    <div className = "flex flex-col gap-1">
+                        <h1>Email</h1>
+                        <input
+                            type="text"
+                            placeholder="enter email"
+                            className="border-2 border-white h-10 w-70 p-3 rounded-xl"
+
+                        />
+                    </div>
+                    <div className = "flex flex-col gap-1">
+                        <h1>Password</h1>
+                        <input
+                            type="password"
+                            placeholder="enter password"
+                            className="border-2 border-white h-10 w-70 p-3 rounded-xl"
+
+                        />
+                    </div>
+                    
+                </div>
+            </div>
             
         </div>
     )
--- apps/user-app/components/Navbar.tsx
+++ apps/user-app/components/Navbar.tsx
@@ -1,16 +1,25 @@
+import Link from "next/link";
+
 export default function Navbar(){
     return(
         <div className="flex flex-row items-center justify-between px-4 border-b-2 border-b-white/40 h-15">
-            <div className="text-2xl font-bold">
-                PAYTM
-            </div>
+            <Link href={"/"}>
+                <div className="text-2xl font-bold">
+                    PAYTM
+                </div>
+            </Link>
             <div className="flex flex-row justify-center items-center gap-4">
-                <button className="border-2 bg-white text-black p-2 font-bold rounded-xl h-10 w-25">
-                    Sign Up
-                </button>
-                <button className="border-2 bg-white text-black p-2 font-bold rounded-xl h-10 w-25">
-                    Sign In
-                </button>
+                <Link href={"/sign-up"}>
+                    <button className="border-2 bg-white text-black p-2 font-bold rounded-xl h-10 w-25">
+                        Sign Up
+                    </button>
+                </Link>
+                <Link href={"/sign-in"}>
+                    <button className="border-2 bg-white text-black p-2 font-bold rounded-xl h-10 w-25">
+                        Sign In
+                    </button>
+                </Link>
+                
             </div>
         </div>
     )
